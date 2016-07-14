ccfc7cf59cc07c59a3775753c7101cbfeb170b37
Bien Angelo Relucano <bien.angelo.relucano@gmail.com>
2016-07-14 17:15:06 +0800

    Update state

--- assets/js/main/main.js
+++ assets/js/main/main.js
@@ -21,22 +21,20 @@
 		var col = $(this).data("id").split("-")[1];
 		var board = $("#board");
 		
-		console.log(col);
+		//console.log(col);
 		
-		// check if bottom cell has no class
+		// Check if bottom cell has no chip
 		for (i = 5; i >= 0; i-- ) {
-			console.log(i);
+			//console.log(i);
 			var id = i + "-" + col;
 			var td = board.find("td[data-id='" + id + "']");
 			
 			if (!td.attr("class")) {
 				td.addClass("chips-" + turn);
 				
-				// Get current id
+				// Update state
 				var r = id.split("-")[0], c = id.split("-")[1];
 				state[r][c] = turn == "yellow" ? 1 : 2;
-				console.log(state);
-				// Update value of current id of array state
 				break;
 			}
 		}
@@ -44,8 +42,4 @@
 		turn = $.Utilities.alternateTurn(turn);
 		
 	});
-    
-	function alternateTurn() {
-		number = number == 1 ? 2 : 1;
-	}
 });
